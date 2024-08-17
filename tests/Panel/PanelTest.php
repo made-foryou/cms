8f69df8a8933a8520e609b9d58b7e93f2b36ee4c
Menno Tempelaar <menno@made-foryou.nl>
2024-08-17 17:37:57 +0200

    :white_check_mark: Add tests for panel access and authentication
    Enhanced the PanelTest to include tests ensuring the panel can be accessed by both anonymous users and authenticated users. This improves test coverage and ensures proper authentication handling for the panel routes.

--- tests/Panel/PanelTest.php
+++ tests/Panel/PanelTest.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use function Pest\Laravel\actingAs;
+
 uses(RefreshDatabase::class);
 
 test('the panel can be accessed', function () {
+    $this->get('/' . config('made-cms.panel.path'))
+        ->assertRedirect();
+});
+
+test('it can be accessed when logged in', function () {
+    $user = \Made\Cms\Models\User::factory()->create();
+
+    actingAs($user, 'made');
+
     $this->get('/' . config('made-cms.panel.path'))
         ->assertSuccessful();
 });
