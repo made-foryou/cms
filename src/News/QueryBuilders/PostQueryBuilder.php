61f2cbb512b703c006f47a03fd71536022a5b4cd
Menno Tempelaar <menno@made-foryou.nl>
2025-02-04 12:20:56 +0100

    Add custom query builder for Post model and related tests
    Integrated `PostQueryBuilder` into the Post model by implementing the custom `newEloquentBuilder` method. Leveraged the `HasPublishingStatusColumn` trait to support status filtering. Added test coverage to ensure correct functionality for published, draft, and status-specific queries.

--- src/News/QueryBuilders/PostQueryBuilder.php
+++ src/News/QueryBuilders/PostQueryBuilder.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Made\Cms\News\QueryBuilders;
 
 use Illuminate\Database\Eloquent\Builder;
+use Made\Cms\Shared\QueryBuilders\HasPublishingStatusColumn;
 
 class PostQueryBuilder extends Builder
 {
-    public function whereStatus() {}
+    use HasPublishingStatusColumn;
 }
