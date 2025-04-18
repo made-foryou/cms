2704a572cbba0e0085e0bb46c93b98cc0e6b58c9
Menno Tempelaar <menno@made-foryou.nl>
2025-04-18 15:45:17 +0200

    ✨ Add Responsable as return type to include inertia.

--- src/App/Http/Controllers/CmsRoutingContract.php
+++ src/App/Http/Controllers/CmsRoutingContract.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Made\Cms\App\Http\Controllers;
 
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
@@ -11,5 +12,5 @@ use Symfony\Component\HttpFoundation\Response;
 
 interface CmsRoutingContract
 {
-    public function __invoke(Request $request, Model $model): View | Response;
+    public function __invoke(Request $request, Model $model): View | Response | Responsable;
 }
