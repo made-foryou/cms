f8931b83554ca0b0bf483ebfeefbde176bb3c343
Menno Tempelaar <menno@made-foryou.nl>
2025-06-06 16:02:00 +0200

    ✅ Refactors news facade to return query builder
    Updates the news facade to return a query builder instance instead of a collection.
    This allows for more flexible and efficient querying of news posts.
    
    The test suite is also updated to reflect this change by calling the `get()` method
    on the query builder.

--- src/News/Facades/MadeNews.php
+++ src/News/Facades/MadeNews.php
@@ -7,9 +7,11 @@ namespace Made\Cms\News\Facades;
 use Illuminate\Support\Facades\Facade;
 use Made\Cms\News\MadeNews as NewsMadeNews;
 use Made\Cms\Page\Models\Page;
+use Made\Cms\News\Models\Post;
+use Made\Cms\News\QueryBuilders\PostQueryBuilder;
 
 /**
- * @method static \Illuminate\Database\Eloquent\Collection news()
+ * @method static PostQueryBuilder<Post> news()
  * @method static ?Page overviewPage()
  *
  * @see \Made\Cms\News\MadeNews
