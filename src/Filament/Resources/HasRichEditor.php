16baf6e36e685667dcc0240331cc405ab15b9d17
mennotempelaar <6905655+mennotempelaar@users.noreply.github.com>
2025-06-10 08:09:10 +0000

    Fix styling

--- src/Filament/Resources/HasRichEditor.php
+++ src/Filament/Resources/HasRichEditor.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Made\Cms\Filament\Resources;
 
-trait HasRichEditor {
+trait HasRichEditor
+{
     public static function toolbarButtons(): array
     {
         return config('made-cms.settings.toolbarButtons', []);
